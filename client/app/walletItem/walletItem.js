9203b531df1f03e060204c0534df02c9bdb2c212
matthieusb <sauboua.beneluz.matthieu@gmail.com>
2016-11-01 19:58:16 +0100

    Added walletItem base files

---
+++ client/app/walletItem/walletItem.js
@@ -0,0 +1,5 @@
+import React from "react"; // eslint-disable-line no-unused-vars
+require("./walletItem.less");
+
+const WalletItem = () => (<div id="walletItem">I am walletItem!</div>);
+export default WalletItem;
---
+++ client/app/walletItem/walletItem.route.js
@@ -0,0 +1,5 @@
+import React from "react"; // eslint-disable-line no-unused-vars
+import { Route } from "react-router";
+import WalletItem from "./walletItem";
+
+export default (<Route path="walletItem" component={WalletItem} />);

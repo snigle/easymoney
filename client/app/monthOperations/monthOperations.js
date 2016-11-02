ef24a122e5edd462991600fd8c30c4aaaf687d66
Ludovic Lamarche <snigle64@gmail.com>
2016-11-02 14:35:59 +0100

    auto scroll to today in operations view

--- client/app/monthOperations/monthOperations.js
+++ client/app/monthOperations/monthOperations.js
@@ -3,8 +3,9 @@ import { connect } from "react-redux";
 import { hashHistory, Link } from "react-router";
 import _ from "lodash";
 import moment from "moment";
+import { Element, scroller } from "react-scroll";
 
-import { FlatButton, DatePicker, TextField, RaisedButton, MenuItem, SelectField, Paper, AppBar, IconButton } from "material-ui";
+import { FlatButton, Paper, AppBar, IconButton } from "material-ui";
 import NavigationClose from "material-ui/svg-icons/navigation/close";
 import muiThemeable from "material-ui/styles/muiThemeable";
 
@@ -28,9 +29,14 @@ class MonthOperations extends React.Component {
     };
   }
 
+  componentDidMount(){
+    setTimeout(() => scroller.scrollTo("today", { containerId : "scrollContainer" }), 250);
+  }
+
   render() {
   //Get this value from wallet
   let currentSum = 100.0;
+  console.log("render");
   return (
     <Paper id="monthOperations">
       <AppBar
@@ -43,13 +49,13 @@ class MonthOperations extends React.Component {
           <FlatButton containerElement={<Link to="/operationForm" />} label="Add"/>
           }
       />
-    <div className="appBody">
+    <div className="appBody element" id="scrollContainer">
       {
         _.map(this.state.operations, ([day,operations]) => (
           <div key={day} >
             <div className="day" style={{ backgroundColor : (day === this.state.today ? this.props.muiTheme.palette.accent1Color : null) }}>
               {moment(day, "YYYY-MM-DD").format("DD MMMM YYYY")}
-              {day === this.state.today ? " (Today)" : ""}
+              {day === this.state.today ? <Element name="today"> (Today)</Element> : ""}
             </div>
             <div>{ operations.map((operation) => {
                 currentSum += operation.value;
